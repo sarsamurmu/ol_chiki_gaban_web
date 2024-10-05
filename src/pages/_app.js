8ca2b2fac6b0ab53ef9a2e89d48834de8f524903
sarsamurmu <sarsamurmu05@gmail.com>
2024-10-05 20:30:45 +0530

    Remove useless stuff and add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.scss";
+import '@/styles/globals.scss'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <Script
+        defer
+        data-cf-beacon='{"token": "bc4b50cd41514d01855ff104fd013e70"}'
+        src='https://static.cloudflareinsights.com/beacon.min.js' />
+    </>
+  )
 }
